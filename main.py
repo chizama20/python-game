b87e3ffd6a100967dc49a8f4567d6c4f56effe8e
chizama20 <133240770+chizama20@users.noreply.github.com>
2025-02-04 16:55:54 -0500

    Update main.py

--- main.py
+++ main.py
@@ -9,12 +9,14 @@ clock = pygame.time.Clock()
 running = True
 
 background_image = pygame.image.load("assets/background.jpg")
-image_width, image_height = background_image.get_size()
-screen = pygame.display.set_mode((image_width, image_height))
+background_image_width, background_image_height = background_image.get_size()
+screen = pygame.display.set_mode((background_image_width, background_image_height))
 
 block_image = pygame.transform.scale(pygame.image.load("assets/block.png"), (50, 50))
-block_x, block_y = image_width // 2, image_height // 2
+block_x, block_y = background_image_width // 2, background_image_height // 2
 speed = 5
+
+apple_image = pygame.transform.scale(pygame.image.load("assets/apple.png"), (50, 50))
 
 while running:
     for event in pygame.event.get():
@@ -31,8 +33,8 @@ while running:
     if keys[K_DOWN]:
         block_y += speed
 
-    block_x = max(50, min(block_x, image_width - 100))  
-    block_y = max(50, min(block_y, image_height - 100))
+    block_x = max(50, min(block_x, background_image_width - 100))  
+    block_y = max(50, min(block_y, background_image_height - 100))
 
 
     screen.blit(background_image, (0, 0))
